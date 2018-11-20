ddf19a4557d1506e588e4c8477d1438de43691b1
Kyle Swanson <swansonk.14@gmail.com>
2018-11-20 15:53:36 -0500

    Bug fix

--- chemprop/train/run_training.py
+++ chemprop/train/run_training.py
@@ -60,7 +60,7 @@ def run_training(args: Namespace, logger: Logger = None) -> List[float]:
     val_data.normalize_features(features_scaler)
     test_data.normalize_features(features_scaler)
 
-    args.train_data_size = len(train_data) if args.prespecified_chunk_dir is not None else args.prespecified_chunks_max_examples_per_epoch
+    args.train_data_size = len(train_data) if args.prespecified_chunk_dir is None else args.prespecified_chunks_max_examples_per_epoch
 
     if args.adversarial or args.moe:
         val_smiles, test_smiles = val_data.smiles(), test_data.smiles()
