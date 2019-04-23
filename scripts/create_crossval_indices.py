aad33f0ba3f0825340eb7fa23789691bb6730884
Kevin Yang <yangk@31-36-24.wireless.csail.mit.edu>
2019-04-23 16:04:14 -0400

    crossval tested and debugged

--- scripts/create_crossval_indices.py
+++ scripts/create_crossval_indices.py
@@ -7,11 +7,20 @@ from copy import deepcopy
 import numpy as np
 
 from chemprop.data.utils import get_data
+from chemprop.data.scaffold import scaffold_to_smiles
 
 def split_indices(all_indices, scaffold=False, data=None, shuffle=True):
     num_data = len(all_indices)
     if scaffold:
-        raise NotImplementedError  # TODO
+        scaffold_to_indices = scaffold_to_smiles(data.mols(), use_indices=True)
+        index_sets = sorted(list(scaffold_to_indices.values()),
+                            key=lambda index_set: len(index_set),
+                            reverse=True)
+        fold_indices = [[] for _ in range(args.num_folds)]
+        for s in index_sets:
+            length_array = [len(fi) for fi in fold_indices]
+            min_index = length_array.index(min(length_array))
+            fold_indices[min_index] += s
     else: # random
         if shuffle:
             random.shuffle(all_indices)
@@ -51,33 +60,44 @@ def create_crossval_splits(args):
         fold_indices = split_indices(all_indices, scaffold=True, data=data)
     else:
         raise ValueError
-    os.makedirs(os.path.join(args.save_dir, args.split_type), exist_ok=True)
+    random.shuffle(fold_indices)
     for i in range(args.test_folds_to_test):
-        with open(os.path.join(args.save_dir, args.split_type, f'fold_{i}', 'split_indices.pckl'), 'wb') as wf:
-            val = fold_indices[(i-1) % args.num_folds]
-            test = fold_indices[i]
-            train = []
-            for j in range(args.num_folds):
-                if j != i and j != (i-1) % args.num_folds:
-                    train = train + fold_indices[j]
-            pickle.dump([train, val, test], wf)
+        for j in range(args.val_folds_per_test):
+            os.makedirs(os.path.join(args.save_dir, args.split_type, f'fold_{i}', f'{j}'), exist_ok=True)
+            with open(os.path.join(args.save_dir, args.split_type, f'fold_{i}', f'{j}', 'split_indices.pckl'), 'wb') as wf:
+                val_idx = (i+j) % args.num_folds
+                val = fold_indices[val_idx]
+                test = fold_indices[i]
+                train = []
+                for k in range(args.num_folds):
+                    if k != i and k != val_idx:
+                        train.append(fold_indices[k])
+                train = np.concatenate(train)
+                pickle.dump([train, val, test], wf)
     
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add_argument('--data_path', type=str, required=True,
                         help='Path to CSV file with dataset of molecules')
-    parser.add_argument('--save_dir', type=str, required=True,
+    parser.add_argument('--save_dir', type=str,
                         help='Path to CSV file where splits will be saved')
     parser.add_argument('--split_type', type=str, choices=['random', 'scaffold', 'time_window'], required=True,
                         help='Random or scaffold based split')
     parser.add_argument('--num_folds', type=int, default=10,
                         help='Number of cross validation folds')
     parser.add_argument('--test_folds_to_test', type=int, default=3,
-                        help='Number of test folds')                    
+                        help='Number of test folds')  
+    parser.add_argument('--val_folds_per_test', type=int, default=3,
+                        help='Number of val folds')                    
     parser.add_argument('--time_folds_per_train_set', type=int, default=3,
                         help='X:1:1 train:val:test for time split sliding window')
     args = parser.parse_args()
+
+    random.seed(0)
+
+    if args.save_dir is None:
+        args.save_dir = os.path.dirname(args.data_path)
     
     if args.split_type == 'time_window':
         create_time_splits(args)
